1ee8c362b38d752aa46c84580b143d650ffd5fca
Lou Montulli <lou@montulli.org>
2015-07-23 11:20:36 -0700

    About update Grooves updates More layout fixes

--- js/grooves.js
+++ js/grooves.js
@@ -11,12 +11,11 @@ if (typeof (grooves) === "undefined") var grooves = {};
 	var root = grooves;
 
 	root.Rock_Grooves = {
-		'8th Note Rock':  '?Div=8&Swing=0&measures=2&showMeasures=1&H=|+xxxxxxo|+xxxxxxo|&S=|--o---o-|--o---o-|&K=|o---oo--|o---oo--|',
-		'16th Note Rock': '?Div=16&Swing=0&measures=2&showMeasures=1&H=|xxxxxxxxxxxxxxxx|xxxxxxxxxxxxxxxx|&S=|----o-------o---|----o-------o---|&K=|o-----o---o--o--|o-----o---o--o--|',
+		'8th Note Rock':  '?Div=8&Tempo=80&Measures=2&showMeasures=1&H=|xxxxxxxx|+xxxxxxo|&S=|--O---O-|--o---o-|&K=|o---oo--|o---oo--|',
+		'16th Note Rock': '?Div=16&Tempo=80&Measures=2&showMeasures=1&H=|xxxxxxxxxxxxxxxx|xxxxxxxxxxxxxxxx|&S=|----O-------O---|----o-------o---|&K=|o-----o---o--o--|o-----o---o--o--|',
 		'Funk': '?Div=16&Title=Funk&Swing=0&measures=2&showMeasures=1&H=|X-x-X-x-X-x-X-x-|X-x-X-x-X-x-X-x-|&S=|-----g-o-o--x--o|-----g-o-o--x--o|&K=|o-o-------o--o--|o-o-------o--o--|',
-		'Basic HipHop': '?Div=8&Swing=0&Title=Basic%20Hip%20Hop&measures=2&showMeasures=1&Tempo=120&H=|xxxxxxxx|xxxxxxxx|&S=|--o---o-|--o---o-|&K=|o--o----|o--o----|',
-        'Upbeat Hi-hats':       '?Div=8&Swing=0&Title=Upbeats&measures=2&showMeasures=1&H=|-x-x-x-x|-x-x-x-x|&S=|--o---o-|--o---o-|&K=|o---o---|o---o---|',
-		'Syncopated Hi-hats': '?Div=16&Swing=0&Title=Syncopated%20hi-hat&measures=2&showMeasures=1&H=|x-xxx-xxx-xxx-xx|x-xxx-xxx-xxx-xx|&S=|----o-------o---|----o-------o---|&K=|o-------o-------|o-------o-------|',
+		'Basic HipHop': '?Div=8&Title=Basic Hip Hop&Tempo=120&Measures=2&showMeasures=1&H=|xxxxxxxx|xxxxxxxx|&S=|--O---O-|--o---o-|&K=|o--o----|o--o----|',
+        	'Syncopated Hi-hats': '?Div=16&Title=Syncopated hi-hat&Tempo=80&Measures=2&showMeasures=1&H=|x-xxx-xxx-xxx-xx|x-xxx-xxx-xxx-xx|&S=|----O-------O---|----o-------o---|&K=|o-------o-------|o-------o-------|',
 		'Train Beat':  '?Div=16&Swing=0&Title=Train%20Beat&Tempo=95&Measures=2&showMeasures=1&H=|----------------|----------------|&S=|ggOgggOgggOggOOg|OOOOOOOOOOOOOOOO|&K=|o-x-o-x-o-x-o-x-|o-------o-------|'
 	};
 
@@ -25,11 +24,13 @@ if (typeof (grooves) === "undefined") var grooves = {};
 		'Half Time Shuffle in 16th notes': '?Div=24&Swing=0&Tempo=85&Measures=2&showMeasures=1&H=|x-xx-xx-xx-xx-xx-xx-xx-x|xxxxxxxxxxxxxxxxxxxxxxxx|&S=|-g--g-Og--g--g--g-Og--g-|------O-----------O-----|&K=|------------------------|o-----------o-----------|',
 		'Purdie Shuffle':    '?Div=12&Title=Purdie%20Shuffle&Swing=0&measures=2&showMeasures=1&H=|x-xx-xx-xx-x|x-xx-xx-xx-x|&S=|-g--g-Og--g-|-g--g-Og--g-|&K=|o----o-----o|o----o-----o|',
 		'Bonham Shuffle':    '?Div=12&Title=Fool%20in%20The%20Rain%20Shuffle&Author=John%20Bonham&Tempo=105&Swing=0&Measures=2&showMeasures=1&H=|x-o+-xx-xx-x|x-xx-xx-xx-x|&S=|-g--g-O---g-|-g--g-Og--g-|&K=|o-o--o-----o|------------|',
-		'Rosanna Shuffle':   '?Div=24&Title=Rosanna%20Shuffle&Author=Jeff%20Pocaro&Tempo=85&Measures=2&showMeasures=1&H=|x-xx-xx-xx-xx-xx-xx-xx-x|x-xx-xx-xx-xx-xx-xx-xx-x|&S=|-g--g-Og--g--g--g-Og--g-|-g--g-Og--g--g--g-Og--g-|&K=|o----o---o----o---o----o|o----o---o----o---o----o|',
+		
+		'Rosanna Shuffle':   '?Div=12&Title=Rosanna Shuffle&Author=Jeff Porcaro&Tempo=170&Measures=2&showMeasures=2&H=|x-xx-xx-xx-x|x-xx-xx-xx-x|&S=|-g--g-Og--g-|-g--g-Og--g-|&K=|o----o---o--|--o---o----o|',
+		//'Rosanna Shuffle in 16ths':   '?Div=24&Title=Rosanna%20Shuffle&Author=Jeff%20Porcaro&Tempo=85&Measures=2&showMeasures=1&H=|x-xx-xx-xx-xx-xx-xx-xx-x|x-xx-xx-xx-xx-xx-xx-xx-x|&S=|-g--g-Og--g--g--g-Og--g-|-g--g-Og--g--g--g-Og--g-|&K=|o----o---o----o---o----o|o----o---o----o---o----o|'
 	};
 
 	root.Jazz = {
-		'Jazz Ride': '?Div=8&Title=Jazz%20Ride%20Pattern&Swing=25&measures=2&showMeasures=1&H=|r-rrr-rr|x-xxx-xx|&S=|--------|--o---o-|&K=|--x---x-|o---o---|',
+		'Jazz Ride': '?Div=12&Tempo=60&Measures=2&showMeasures=1&H=|r--r-rr--r-r|xxxxxxxxxxxx|&S=|------------|--O---------|&K=|---x-----x--|o-----------|',
 	};
 
 	root.Latin = {
@@ -38,7 +39,7 @@ if (typeof (grooves) === "undefined") var grooves = {};
 		'Samba Fast':  '?Div=16&Title=Samba&Tempo=120&Swing=0&measures=2&showMeasures=1&H=|x-xxx-xxx-xxx-xx|x-xxx-xxx-xxx-xx|&S=|--o--o---o--o-o-|--o--o---o--o-o-|&K=|o-xoo-xoo-xoo-xo|o-xoo-xoo-xoo-xo|',
 		'Tumbao':      '?Div=16&Title=Tumbao&Swing=0&measures=2&showMeasures=1&H=|x-xxx-xxx-xxx-xx|x-xxx-xxx-xxx-xx|&S=|----------------|----------------|&K=|o-xo--X-o-xo--X-|o-xo--X-o-xo--X-|',
 		'Baiao':       '?Div=16&Title=baiao&Swing=0&measures=2&showMeasures=1&H=|xxX-xxX-xxX-xxX-|xxX-xxX-xxX-xxX-|&S=|------o-------o-|------o-------o-|&K=|o--o----o--o---o|o--o----o--o---o|',
-		'Songo':       '?Div=16&Title=Songo&Swing=0&measures=2&showMeasures=1&H=|x---x---x---x---|x---x---x---x---|&S=|--o--o-o-oo--o-o|--o--o-o-oo--o-o|&K=|---o--o----o--o-|---o--o----o--o-|'
+		'Songo':       '?Div=16&Title=Songo&Tempo=80&Measures=2&showMeasures=1&H=|x---x---x---x---|x---x---x---x---|&S=|--O--g-O-gg--g-g|--o--o-o-oo--o-o|&K=|---o--o----o--o-|---o--o----o--o-|'
 	};
 	
 	root.FullArray = {"Rock grooves":     root.Rock_Grooves,
