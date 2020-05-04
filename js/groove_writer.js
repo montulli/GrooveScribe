038f04f3ed6dcbd7047e106674c4640a71086a28
Lou Montulli <lou@montulli.org>
2020-05-04 16:27:23 -0700

    Add copy button right sticking highlight color

--- js/groove_writer.js
+++ js/groove_writer.js
@@ -72,7 +72,7 @@ function GrooveWriter() {
 	var constant_hihat_note_off_color_hex = "#CCC";
 	var constant_hihat_note_off_color_rgb = 'rgb(204, 204, 204)'; // grey
 	var constant_note_hidden_color_rgb = "transparent";
-	var constant_sticking_right_on_color_rgb = "rgb(204, 101, 0)";
+	var constant_sticking_right_on_color_rgb = "rgb(36, 132, 192)";
 	var constant_sticking_left_on_color_rgb = "rgb(57, 57, 57)";
 	var constant_sticking_both_on_color_rgb = "rgb(57, 57, 57)";
 	var constant_sticking_count_on_color_rgb = "rgb(57, 57, 57)";
